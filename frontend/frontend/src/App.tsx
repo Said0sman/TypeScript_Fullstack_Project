3391dbf6564d6407f423b6e1d18fffc8aadefca2
Said Osman <said_henry@hotmail.com>
2021-11-09 12:39:00 +0100

    New updates style components

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -5,16 +5,35 @@ import styled from "styled-components";
 function App() {
   return (
     <CoverWrap>
-   <h1>React with TypeScript!</h1>
+        <SectionTwoColumns>
+            <Article>
+                <h1>React with TypeScript!</h1>
+            </Article>
+        </SectionTwoColumns>
     </CoverWrap>
   );
 }
 const CoverWrap = styled.div`
-background-color: khaki;
+background-color: blanchedalmond;
 `
 const H1 = styled.h1 `
 font-size: 2em;
 `
-
+const Article = styled.article `
+padding: 1em;
+  border: 1px solid black;
+  border-radius: 1em;
+  text-align: center;
+  background-color: darkcyan;
+`
+const SectionTwoColumns = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1em;
+  padding: 0 1em 1em 1em;
+  &:first-child {
+    padding-top: 1em;
+  }
+`
 
 export default App;
